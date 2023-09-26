da1b4584ddf205dec121bc56ff860c0de6b09564
Sudhir <yadavsudhir405@gmail.com>
2023-09-26 20:40:43 +0530

    Third commit

--- src/main/java/com/example/testingspringapp/TestingSpringAppApplication.java
+++ src/main/java/com/example/testingspringapp/TestingSpringAppApplication.java
@@ -6,10 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestingSpringAppApplication {
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 60e779c (First commit)
     public static void main(String[] args) {
         SpringApplication.run(TestingSpringAppApplication.class, args);
     }
