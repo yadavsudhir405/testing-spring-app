08f0c7d4cbf6fdbdecd8f330ccb6ae8298e22a0d
Sudhir <yadavsudhir405@gmail.com>
2023-10-05 10:59:51 +0530

    Unit Testing | Context Management with Inheritance example to show test fail when inheritLocation = false on @SpringJunitConfig

--- src/test/java/com/example/testingspringapp/config/ContextConfigurationViaInheritancePropertySource.java
+++ src/test/java/com/example/testingspringapp/config/ContextConfigurationViaInheritancePropertySource.java
@@ -11,7 +11,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 /**
  * Since this class is extending a class having required annotation(@SpringJunitConfig) to run test it doesn't need further declare those
  * annotation to run the test.
+ *
+ *  When designing a Base config class for Test, Base class should be marked as Abstract
+ *  ContextConfigurationViaInheritancePropertySource's test would fail when it declares that do not inherit location. This is because , Spring will
+ *  not be able to resolve dependancy DbConfigProperty with the absence of it's dependent properties
  */
+
+
+//@SpringJUnitConfig(inheritLocations = false) with this test will fail OR @TestPropertySource(inheritLocation = false) OR @TestPropertySource(inheritLocation = false)
 public class ContextConfigurationViaInheritancePropertySource extends BaseTestConfigWithPropertyResource{
 
     @Test
@@ -22,9 +29,9 @@ public class ContextConfigurationViaInheritancePropertySource extends BaseTestCo
     }
 }
 
-@SpringJUnitConfig(DbConfigProperty.class)
+@SpringJUnitConfig(value = DbConfigProperty.class)
 @EnableConfigurationProperties
 @TestPropertySource(locations = "classpath:db.test.properties")
-class BaseTestConfigWithPropertyResource {
+abstract class BaseTestConfigWithPropertyResource {
 
 }
